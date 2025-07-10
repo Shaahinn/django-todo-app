3122209a190e0ed9566a4b3a615bdcf9a64f869e
Maaman <21mcs18@meaec.edu.in>
2025-07-10 23:25:49 +0530

    Added task form, template, normal user setup, and URLs

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:  # avoid saving empty tasks
+            Task.objects.create(title=title)
+        return redirect('/')  # after saving, reload the page
+
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
