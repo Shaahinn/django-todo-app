e32d5ede80dfb5241a70a4015734b600b4f5ae80
Maaman <21mcs18@meaec.edu.in>
2025-07-13 19:22:37 +0530

    Added complete & delete task features with styling

--- todo/views.py
+++ todo/views.py
@@ -10,3 +10,16 @@ def task_list(request):
 
     tasks = Task.objects.all()
     return render(request, 'todo/task_list.html', {'tasks': tasks})
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('/')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('/')
